2b7c99494d8efd9c6e8cc196ad0bdecf0bd8a431
Silver1078682 <ShinySmoothSilver@outlook.com>
2026-01-27 10:44:02 +0800

    Remove legacy files

--- tools/debug/unit_test/tests/scout.gd
+++
@@ -1,5 +0,0 @@
-extends Tester
-
-func run() -> void:
-	Attack.create_from_name("ScoutAircraft").launch(Map.instance.get_coords())
-	pass
--- tools/debug/unit_test/tests/ship_test.gd
+++
@@ -1,6 +0,0 @@
-extends Tester
-
-func _run() -> void:
-	var coords = get_spawn_rect().get_coords().keys()
-	for i in TYPE_COUNT:
-		_add_warship(coords[i], Warship.NAMES[i])
