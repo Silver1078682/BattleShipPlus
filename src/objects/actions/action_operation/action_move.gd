9dfa4b84f360b8160c1026748a794fb3d016d164
Silver1078682 <ShinySmoothSilver@outlook.com>
2026-01-31 15:42:16 +0800

    Feat: feedback on ActionMove reverted failed

--- src/objects/actions/action_operation/action_move.gd
+++ src/objects/actions/action_operation/action_move.gd
@@ -25,15 +25,30 @@ func _committed() -> bool:
 
 
 func _reverted() -> bool:
+	if Player.fleet.has_ship_at(_previous_coord):
+		_revert_failure_anim()
+		return false
 	return _move_ship_to(_previous_coord)
+
+
+const TILE_TEXTURE: Texture = preload("uid://dh4opsme32878")
+const REVERT_FAILURE_HINT_COLOR := Color.ORANGE_RED
+
+
+func _revert_failure_anim():
+	Anim.pop_up("OCCUPIED")
+	var hint := Sprite2D.new()
+	hint.texture = TILE_TEXTURE
+	hint.modulate = REVERT_FAILURE_HINT_COLOR
+	hint.position = Map.coord_to_pos(_previous_coord)
+	Game.instance.add_child(hint)
+	await Anim.fade_out(hint).finished
+	hint.queue_free()
 
 
 #-----------------------------------------------------------------#
 func _started() -> void:
 	pass
-	#if not _line:
-	#_line = Line2D.new()
-	#_ship.add_child(_line)
 
 
 #-----------------------------------------------------------------#
